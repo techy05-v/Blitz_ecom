05da9c2f14ee2053d547085b45ecdf62a4562b04
techy05-v <ravivishnu929@gmail.com>
2025-02-11 09:48:52 +0530

    m

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
     const uri = process.env.MONGO_URI||`mongodb://127.0.0.1:27017/application`; 
     console.log('Attempting to connect with URI:', uri);
     const connection = await mongoose
-    .connect(`mongodb://127.0.0.1:27017/application`)
+    .connect(uri)
     .then(async() => {
       console.log(
         "mongodb connected"
